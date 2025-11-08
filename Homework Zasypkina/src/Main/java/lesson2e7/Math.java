6c2e1d389adec9b419bc99958b97994282261be9
Zasypkina <svet.zsp@yandex.ru>
2025-11-08 19:49:26 +0400

    исправлено ДЗ 2.7 testNG

--- Homework Zasypkina/src/Main/java/lesson2e7/Math.java
+++ Homework Zasypkina/src/Main/java/lesson2e7/Math.java
@@ -1,22 +1,20 @@
 package lesson2e7;
 
 //Напишите программу, сравнивающую два целых числа.
-public class Match {
+public class Math {
     public int sum;
     private int m1;
     private int m2;
 
-    public Match(int m1, int m2) {
+    public Math(int m1, int m2) {
         this.m1 = m1;
         this.m2 = m2;
     }
 
-    public boolean isMatch() {
-        if (m1 == m2) {
-            return true;
-        } else {
-            return false;
-        }
+    public int isMatch() {
+        if (m1 == m2) {return 0;}
+        else if (m1 < m2) {return -1;}
+        else {return 1;}
     }
 
     public int sum() {
@@ -33,10 +31,5 @@ public class Match {
 
     public int devide() {
         return m1 / m2;
-    }
-
-    static void main() {
-        Match intMatch = new Match(4, 5);
-        System.out.println(intMatch.isMatch());
     }
 }
--- Homework Zasypkina/src/test/java/lesson2e7Tests/TestNG.java
+++ Homework Zasypkina/src/test/java/lesson2e7Tests/TestNG.java
@@ -1,18 +1,22 @@
 package lesson2e7Tests;
 
 import Lesson_4.ex2.Triangle;
-import lesson2e7.Match;
+import lesson2e7.Math;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG {
-    Match match1;
+    Math match1;
+    Math match2;
+    Math match3;
     Triangle t3;
 
     @BeforeMethod
     public void setUp() {
-        match1 = new Match(20, 20);
+        match1 = new Math(20, 20);
+        match2 = new Math(3, 5);
+        match3 = new Math(13, 8);
     }
 
     @Test
@@ -29,8 +33,20 @@ public class TestNG {
     }
 
     @Test
-    public void testMatch() {
-        boolean result = match1.isMatch();
-        Assert.assertTrue(result, "числа не соответствуют друг другу");
+    public void testMatchEquals() {
+        int result = match1.isMatch();
+        Assert.assertEquals(result, 0, "Числа эквивалентны друг другу, поэтому результат = '0'");
+    }
+
+    @Test
+    public void testMatchLess() {
+        int result = match2.isMatch();
+        Assert.assertEquals(result, -1, "Первое число меньше второго, поэтому результат = '-1'");
+    }
+
+    @Test
+    public void testMatchMore() {
+        int result = match3.isMatch();
+        Assert.assertEquals(result, 1, "Первое число больше второго, поэтому результат = '1'");
     }
 }
